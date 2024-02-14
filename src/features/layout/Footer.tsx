dc7887b0b9ee365fee27018e152362cdc92f01a6
MKL DEVOPS <88685078+mkldevops@users.noreply.github.com>
2024-02-14 21:53:19 +0000

    feat: #4 make the footer (#7)

--- src/features/layout/Footer.tsx
+++ src/features/layout/Footer.tsx
@@ -1,3 +1,56 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Github, Linkedin, Twitter } from "lucide-react";
+import Link from "next/link";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="container">
+      <div className="custom-screen pt-16">
+        <div className="mt-10 py-10 border-t items-center justify-between sm:flex">
+          <p className="text-gray-600">
+            © 2023 MKL Devops. All rights reserved.
+          </p>
+          <div className="flex items-center gap-x-6 text-gray-400 mt-6">
+            <Link
+              href="https://github.com/mkldevops"
+              aria-label="social media"
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                buttonVariants({ variant: "default" }),
+                "rounded-full p-2"
+              )}
+            >
+              <Github size={22} />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/fahari"
+              aria-label="social media"
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                buttonVariants({ variant: "default" }),
+                "rounded-full p-2"
+              )}
+            >
+              <Linkedin size={24} />
+            </Link>
+            <Link
+              href="#"
+              aria-label="social media"
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                buttonVariants({ variant: "default" }),
+                "rounded-full p-2"
+              )}
+            >
+              <Twitter size={24} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
