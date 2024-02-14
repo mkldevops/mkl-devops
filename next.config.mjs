f991c6098e24b1ea78093fa93e9f9c87f6703bb2
Fahari <mkl.devops@gmail.com>
2024-02-14 23:15:45 +0000

    build: #9 install next pwa

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 export default withPWA(nextConfig);
